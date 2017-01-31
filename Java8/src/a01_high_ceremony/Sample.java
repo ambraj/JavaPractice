f9f01a8237b73a277c22becc3a584fb380dc0f12
skhanyus <saima.khanyusufi@openbet.com>
2017-01-31 14:36:03 +0530

    commit from linux system

--- Java8/src/a01_high_ceremony/Sample.java
+++ Java8/src/a01_high_ceremony/Sample.java
@@ -1,10 +1,10 @@
 package a01_high_ceremony;
 
 public class Sample {
-  public static void main(String[] args) {
-    Thread th = new Thread(() -> System.out.println("In another thread"));
-    th.start();
-    
-    System.out.println("In main");
-  }
+    public static void main(String[] args) {
+        Thread th = new Thread(() -> System.out.println("In another thread"));
+        th.start();
+
+        System.out.println("In main");
+    }
 }
