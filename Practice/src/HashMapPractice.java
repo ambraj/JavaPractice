23e16441eb3c04b335e4d91c81cf6dc963b60bb8
Ambuj Rajput <ambraj@gmail.com>
2018-03-20 22:38:30 +0530

    hashmap practice

--- Practice/src/HashMapPractice.java
+++ Practice/src/HashMapPractice.java
@@ -1,20 +1,23 @@
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HashMapPractice {
 
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new ConcurrentHashMap<>();
         map.put("one", 1);
         map.put("two", 2);
         map.put("three", 3);
+
+        //        map = Collections.synchronizedMap(map);
 
         // iterating entry set
         Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
         for (Map.Entry<String, Integer> entry : entrySet) {
             System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
+            map.put("four", 4);
         }
 
         for (Integer value : map.values()) {
