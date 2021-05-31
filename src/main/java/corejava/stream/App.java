1c2fdf3387e14f8667526c6e7db8cb301e9ec4ac
Ambuj Rajput <arajput@ingenuitygaming.com>
2021-05-31 11:33:34 +0530

    stream api

---
+++ src/main/java/corejava/stream/App.java
@@ -0,0 +1,30 @@
+package corejava.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+
+    private static List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 800, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 300, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+
+    public static void main(String[] args) {
+
+        menu.stream()
+                .filter(c -> c.getCalories() <= 350)
+                .map(Dish::getName)
+                .forEach(System.out::println);
+
+    }
+
+}
